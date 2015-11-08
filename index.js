4868706098ea467a81d1cb91ccc930dd3e21762a
Yi Yan <yiyantay@gmail.com>
2015-11-07 21:32:53 -0500

    twilio

--- index.js
+++ index.js
@@ -72,71 +72,93 @@ var keys = db.get('keys');
 //var favicon = require('serve-favicon'); //serve favicon for site
 //var munge = require('munge'); //obfuscate email
 
-
+var twilio = require('twilio')('ACd566c2614fae1998ae89a275952b4ccc', 'dfeacbb442ea9601ae93a0c3ff505d54');
 
 var numVisits = 0;
 
 app.use(bodyParser.json());
 //app.use('/deposits', bodyParser.urlencoded({extended : false}));
-
+app.configure(function() {
+  /* Configure your express app... */
+  app.use(express.urlencoded());
+});
 app.listen(process.env.PORT || 3000, function() {
-    console.log("Node app is running");
+  console.log("Node app is running");
 });
 
-app.get('/', function(req, res){
-    numVisits ++;
-    res.set('text/plain');
-    //res.send("Hi Brandon!!! You're visiting for the " + numVisits + " time!");
-    res.send("Result of hashing " + message + " is " + SHA256(message));
-    res.end();
-    console.log("GET Request received");
+app.get('/', function(req, res) {
+  numVisits++;
+  res.set('text/plain');
+  //res.send("Hi Brandon!!! You're visiting for the " + numVisits + " time!");
+  res.send("Result of hashing " + message + " is " + SHA256(message));
+  res.end();
+  console.log("GET Request received");
 
-    console.log("Result of hashing " + message + " is " + SHA256(message));
+  console.log("Result of hashing " + message + " is " + SHA256(message));
 
-    console.log("All users: ");
-    console.log();
-    users.find({}, function(err, res) {
-      console.log(res);  
-    });
+  console.log("All users: ");
+  console.log();
+  users.find({}, function(err, res) {
+    console.log(res);
+  });
 });
 
 // Transaction requests sent by SMS
 app.post('/deposits', function(req, res) {
-	var body = req.body;
-//	console.log("Received deposit string " + JSON.stringify(req.body));
-//	res.send();
-	res.set('text/plain').send('Your encrypted message is ' + SHA256(body)).end();
+  var body = req.body;
+  //	console.log("Received deposit string " + JSON.stringify(req.body));
+  //	res.send();
+  res.set('text/plain').send('Your encrypted message is ' + SHA256(body)).end();
 });
 
 
 // Registration of new user
-app.post('/users/new', function(req, res){
+app.post('/users/new', function(req, res) {
 
-    var newUser = req.body;
-    var keyId;
-    var key;
-    console.log("Received JSON object for new user:");
-    console.log(newUser);
-    newUser['balance'] = 0;
-    users.insert(newUser, function(err, reply) {
-        console.log("Added new user to database");
-        keyId = reply['_id'];
-        console.log("Key id is " + keyId);
-    });
-    key = new NodeRSA({b: 512});
-//    key.generateKeyPair((Math.floor((Math.random() * 10)* + 1))*8);
-    key.generateKeyPair();
-    var privKey = key.getPrivatePEM();
-    var pubKey = key.getPublicPEM();
-    //var info = "Your public key is:\n" + pubKey;
-    var info = {'Key' : pubKey, 'ID' : keyId};
-    res.set('app/json').send(info);
-    res.end();
-    var keyInfo = {'id' : keyId, 'Private Key' : privKey};
-    keys.insert(keyInfo, function(err, rep) {
-        console.log('Saved new private key into database');
-        console.log(rep);
-    });
+  var newUser = req.body;
+  var keyId;
+  var key;
+  console.log("Received JSON object for new user:");
+  console.log(newUser);
+  newUser['balance'] = 0;
+  users.insert(newUser, function(err, reply) {
+    console.log("Added new user to database");
+    keyId = reply['_id'];
+    console.log("Key id is " + keyId);
+  });
+  key = new NodeRSA({
+    b: 512
+  });
+  //    key.generateKeyPair((Math.floor((Math.random() * 10)* + 1))*8);
+  key.generateKeyPair();
+  var privKey = key.getPrivatePEM();
+  var pubKey = key.getPublicPEM();
+  //var info = "Your public key is:\n" + pubKey;
+  var info = {
+    'Key': pubKey,
+    'ID': keyId
+  };
+  res.set('app/json').send(info);
+  res.end();
+  var keyInfo = {
+    'id': keyId,
+    'Private Key': privKey
+  };
+  keys.insert(keyInfo, function(err, rep) {
+    console.log('Saved new private key into database');
+    console.log(rep);
+  });
 });
 
+app.get('/twilio', function(req, res) {
+  if (twilio.validateExpressRequest(req, 'dfeacbb442ea9601ae93a0c3ff505d54')) {
+    var resp = new twilio.TwimlResponse();
+    console.log("twilio req is", req);
+    resp.say('express sez - hello twilio!');
 
+    res.type('text/xml');
+    res.send(resp.toString());
+  } else {
+    res.send('you are not twilio.  Buzz off.');
+  }
+});
